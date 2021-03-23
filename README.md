f19bc0b8688aeb1fea7225b6d074827a4d3fd792
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-03-23 19:52:14 +0800

    Change entire website

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # [MarsRon.github.io](https://marsron.github.io)
-Looks like you found my GitHub page!
 
-To do:
-* None
+Looks like you found my GitHub page!
--- js/preload.js
+++
@@ -1,7 +0,0 @@
-function preload() {
-	var e = document.getElementsByClassName("preload");
-	for (var i = 0; i < e.length; i++)
-		e[i].classList.remove("preload");
-}
-preload();
-document.addEventListener("DOMContentLoaded", preload);
