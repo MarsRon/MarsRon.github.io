354d32cbd5c7d02bd9b75221405c4f8aea9c7758
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-04-21 23:58:58 +0800

    Update pendulum | Optimise homepage
    	modified:   css/index.css
    	modified:   css/pendulum.css
    	modified:   index.html
    	modified:   js/pendulum.js
    	modified:   pendulum.html
    
    	modified:   README.md
    	modified:   css/index.css
    	modified:   css/pendulum.css
    	modified:   index.html
    	modified:   js/index.js
    	modified:   js/pendulum.js
    modified:   pendulum.html

--- js/pendulum.js
+++ js/pendulum.js
@@ -5,13 +5,13 @@ const gravity = 0.35,
 	length = 200,
 	origin = { x: 175, y: 5 };
 
-let angle = Math.PI / 5,
+let angle = Math.PI / -5,
 	angleV = 0,
 	angleA = 0,
 	bob = { x: 0, y: 0 };
 
 function setup() {
-	createCanvas(350, 300);
+	createCanvas(350, 300).parent("canvas");
 }
 
 function draw() {
@@ -24,7 +24,7 @@ function draw() {
 	bob.x = length * sin(angle) + origin.x;
 	bob.y = length * cos(angle) + origin.y;
 
-	background(0);
+	background(36);
 	stroke(255);
 	strokeWeight(2);
 	line(origin.x, origin.y, bob.x, bob.y);
